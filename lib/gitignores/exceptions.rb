d2f146ece51dc93b2f87ddf05937ad2f56bca378
Johnathan Gilday <me@johnathangilday.com>
2013-06-23 17:55:47 -0400

    Fixed up error handling when a gitignore cannot be found

--- lib/gitignores/exceptions.rb
+++ lib/gitignores/exceptions.rb
@@ -1,6 +1,6 @@
 module Gitignores
   class GitignoreNotFoundException < RuntimeError
-    attr :ignore_name
+    attr_accessor :ignore_name
     def initialize(ignore_name)
       @ignore_name = ignore_name
     end
