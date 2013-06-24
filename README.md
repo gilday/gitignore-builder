c8fb7bf792a29f3980fc919ae1f1433410f55adf
Johnathan Gilday <me@johnathangilday.com>
2013-06-23 22:12:27 -0400

    Fixes #4 Introduced the concept of commands. Added the show command and refactored the old functionality into the add command

--- README.md
+++ README.md
@@ -8,8 +8,14 @@ Tool for building a .gitignore from community templates at [github/gitignore](ht
 
 ## Usage
 
-Give gitignores a list of .gitignores to append to the `.gitignore` in the current directory. Example:
+**add** append a list of .gitignores to the `.gitignore` in the current directory. Example:
 
-    gitignores Java IntelliJ Eclipse
+    gitignores add Java IntelliJ Eclipse
 
 Adds gitignores for Java, IntelliJ, Eclipse to CURRENT_DIR/.gitignore
+
+**show** prints a concatenation of .gitignores to std_out
+
+    gitignores show Java IntelliJ Eclipse
+
+Prints gitignores for Java, IntelliJ, Eclipse to standard out
--- lib/gitignores.rb
+++ lib/gitignores.rb
@@ -3,21 +3,29 @@ require 'gitignores/builder'
 require 'gitignores/fetcher'
 
 module Gitignores
+  class Gitignores
 
-  def self.main(ignores, options = {})
-    out_file = File.open('.gitignore', 'a')
-    options = {
-      :update_ignores => false,
-      :ignores_dir => ENV['HOME'] + '/.gitignores',
-      :out => out_file
-    }.merge(options)
+    def initialize(options = {})
+      options = {
+        :update_ignores => false,
+        :ignores_dir => ENV['HOME'] + '/.gitignores'
+      }.merge(options)
 
-    builder = GitignoreBuilder.new
-    builder.ignores_dir = options[:ignores_dir]
-    fetcher = GitignoreFetcher.new
-    fetcher.update_ignores = options[:update_ignores]
+      @builder = GitignoreBuilder.new
+      @builder.ignores_dir = options[:ignores_dir]
+      fetcher = GitignoreFetcher.new
+      fetcher.update_ignores = options[:update_ignores]
 
-    fetcher.fetch_gitignores options[:ignores_dir]
-    builder.build(ignores, options[:out])
+      fetcher.fetch_gitignores options[:ignores_dir]
+    end
+
+    def add(ignores)
+      out_file = File.open('.gitignore', 'a')
+      @builder.build(ignores, out_file)
+    end
+
+    def show(ignores)
+      @builder.build(ignores, STDOUT)
+    end
   end
 end
